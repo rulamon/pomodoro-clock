12f98961b3e9ad042fa5d9ef3f50d86400ef6bdb
rulamon <ruben.lamon@gmail.com>
2019-02-06 16:39:07 +0100

    basic environment set up

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class App extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div id="main-container">
+            Hallo Wereld
+            </div>
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }//,
+            // {
+            //     test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            //     use: [{
+            //         loader: 'file-loader',
+            //         options: {
+            //             name: '[name].[ext]',
+            //             outputPath: 'fonts/'
+            //         }
+            //     }]
+            // }
+
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
